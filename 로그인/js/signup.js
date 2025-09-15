cf2993c1153e88de149b08f2a5b45d5d5564acba
donghyeonyang984-collab <donghyeonyang984@gmail.com>
2025-09-15 13:52:45 +0900

    Delete 로그인 directory

--- 로그인/js/signup.js
+++
@@ -1,7 +0,0 @@
-// 성별 버튼 선택 효과
-document.querySelectorAll('.gender-btn').forEach(btn => {
-    btn.addEventListener('click', function() {
-        document.querySelectorAll('.gender-btn').forEach(b => b.classList.remove('active'));
-        this.classList.add('active');
-    });
-});
