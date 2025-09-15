9200a7d0a3eb5b3c0280f50292e1aed74c1f7186
kjjjk789 <100752699+kangsungung@users.noreply.github.com>
2025-09-15 11:04:14 +0900

    Delete projectC_src/main directory

--- projectC_src/main/java/com/example/projectc/repo/EnrollmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projectc.repo;
-
-import com.example.projectc.entity.Enrollment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnrollmentRepository extends JpaRepository<Enrollment,Long> {
-}
