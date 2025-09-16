2d819b5608c48f8ac8bf92b645e64b88df10885f
minu222 <113432255+minu222@users.noreply.github.com>
2025-09-16 13:41:30 +0900

    Update UserRepository.java
    @Repository 추가

--- 이민우/src/lmsProjectC/src/main/java/com/lms/lmsprojectc/repository/UserRepository.java
+++ 이민우/src/lmsProjectC/src/main/java/com/lms/lmsprojectc/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.lms.lmsprojectc.repository;
 import com.lms.lmsprojectc.domain.Role;
 import com.lms.lmsprojectc.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     // 특정 Role(예: instructor) 만 가져오기
