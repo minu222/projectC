0ec0eebb43716dc4c3a35a168486aaf3603ebdb6
kjjjk789 <100752699+kangsungung@users.noreply.github.com>
2025-09-16 12:07:57 +0900

    Delete 강성웅/projectC_src/main directory

--- 강성웅/projectC_src/main/java/com/example/projectc/repo/InstructorProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projectc.repo;
-
-import com.example.projectc.entity.InstructorProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstructorProfileRepository extends JpaRepository<InstructorProfile,Long> {
-}
--- 강성웅/projectC_src/main/java/com/example/projectc/repo/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.projectc.repo;
-
-import com.example.projectc.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
